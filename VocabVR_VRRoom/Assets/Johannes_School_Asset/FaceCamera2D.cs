4694664a8847fece2d077a8f38ffd012ed5b545b
JohannesUllmann <johannes-ullmann@web.de>
2024-06-12 18:16:12 +0200

    changed logic for camera
    - minor changes to the camera logic (sprites looking at you)

--- VocabVR_VRRoom/Assets/Johannes_School_Asset/FaceCamera2D.cs
+++ VocabVR_VRRoom/Assets/Johannes_School_Asset/FaceCamera2D.cs
@@ -8,19 +8,13 @@ public class FaceCamera2D : MonoBehaviour
 
     void Start()
     {
-        // Find the camera in the scene (assuming there's only one)
         cameraTransform = Camera.main.transform;
     }
 
     void Update()
     {
-        // Calculate direction from sprite to camera
         Vector3 direction = cameraTransform.position - transform.position;
-
-        // We only care about the horizontal direction
         direction.z = 0.0f;
-
-        // Calculate the angle and apply it to the sprite
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
