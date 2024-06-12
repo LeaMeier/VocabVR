f2e33a90adb981f97f798e517e8cfafebb213108
JohannesUllmann <johannes-ullmann@web.de>
2024-06-12 17:35:19 +0200

    Logic for 2D Sprites
    - Sprites will look directly to the camera when moving around

---
+++ VocabVR_VRRoom/Assets/Johannes_School_Asset/FaceCamera2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera2D : MonoBehaviour
+{
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        // Find the camera in the scene (assuming there's only one)
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        // Calculate direction from sprite to camera
+        Vector3 direction = cameraTransform.position - transform.position;
+
+        // We only care about the horizontal direction
+        direction.z = 0.0f;
+
+        // Calculate the angle and apply it to the sprite
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
