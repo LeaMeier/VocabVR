5cf3072fe1f05b07e56c1d4758cc581352e5a984
JohannesUllmann <johannes-ullmann@web.de>
2024-06-18 20:00:12 +0200

    Sprite script adapted and other changes made
    - Sprite script reprogrammed for correct execution of the desired logic
    - Sprites removed and new ones added
    - Wall collider made larger
    - Supermarket door positioning changed

---
+++ VocabVR_VRRoom/Assets/Johannes_School_Asset/FacePlayerVR.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayerVR : MonoBehaviour
+{
+    public Transform playerCamera;
+
+    void Update()
+    {
+        // Überprüfen, ob die Spieler-Kamera gesetzt ist
+        if (playerCamera != null)
+        {
+            // Berechnen Sie die Richtung von diesem Objekt zur Spieler-Kamera
+            Vector3 direction = playerCamera.position - transform.position;
+
+            // Ignorieren Sie die Y-Achse, um ein Kippen zu verhindern
+            direction.y = 0;
+
+            // Berechnen Sie die Rotation, die dieses Objekt in Richtung der Kamera zeigt
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            // Setzen Sie die Rotation dieses Objekts
+            transform.rotation = rotation;
+        }
+    }
+}
