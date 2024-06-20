7c6786677cb1b374b9e8cda42c66e1bc67c51445
Kristina Koller <krissy.koller98@gmail.com>
2024-06-20 22:35:51 +0200

    change script

--- VocabVR_VRRoom/Assets/VocabVRLogic/Interaction/InteractableObject.cs
+++ VocabVR_VRRoom/Assets/VocabVRLogic/Interaction/InteractableObject.cs
@@ -1,6 +1,7 @@
 using LMNT;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InteractableObject : MonoBehaviour
 {
@@ -21,7 +22,6 @@ public class InteractableObject : MonoBehaviour
     public Button wrongAnswer;
     public Button wrongAnswer2;
     public Button wrongAnswer3;
-    private int clickCount = 1;
     private LMNTSpeech speech;
 
     void Start()
@@ -50,47 +50,38 @@ public class InteractableObject : MonoBehaviour
         speech = GetComponent<LMNTSpeech>();
     }
 
-    void Update()
+    public void Update()
     {
-        // Check for controller input (e.g., the "A" button on an Xbox controller)
-        if (Input.GetButtonDown("Fire1")) // "Fire1" is typically mapped to the "A" button on controllers
+        if(Input.GetButtonDown("Fire1"))
         {
-            OnControllerClick();
-        }
-    }
+            if (exclamationMarkRed.activeSelf)
+            {
+                exclamationMarkRed.SetActive(false);
+                readCard.SetActive(true);
+            }
 
-    void OnControllerClick()
-    {
-        if (clickCount == 1)
-        {
-            exclamationMarkRed.SetActive(false);
-            readCard.SetActive(true);
-        }
+            if (exclamationMarkOrange.activeSelf)
+            {
+                exclamationMarkOrange.SetActive(false);
+                listenCard.SetActive(true);
+            }
 
-        if (clickCount == 2)
-        {
-            exclamationMarkOrange.SetActive(false);
-            listenCard.SetActive(true);
-        }
-
-        if (clickCount == 3)
-        {
-            exclamationMarkGreen.SetActive(false);
-            checkBackCard.SetActive(true);
-            clickCount++;
+            if (exclamationMarkGreen.activeSelf)
+            {
+                exclamationMarkGreen.SetActive(false);
+                checkBackCard.SetActive(true);
+            }
         }
     }
 
     void CloseReadCard()
     {
-        clickCount++;
         readCard.SetActive(false);
         exclamationMarkOrange.SetActive(true);
     }
 
     void CloseListenCard()
     {
-        clickCount++;
         listenCard.SetActive(false);
         exclamationMarkGreen.SetActive(true);
     }
@@ -120,7 +111,6 @@ public class InteractableObject : MonoBehaviour
     void CloseWrongAnswerCard()
     {
         wrongAnswerCard.SetActive(false);
-        clickCount = 1;
-        exclamationMarkGreen.SetActive(true);
+        exclamationMarkRed.SetActive(true);
     }
 }
