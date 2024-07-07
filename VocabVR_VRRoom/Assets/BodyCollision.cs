8fb79189522050708ff0acdeb34c79e301f6aa42
JohannesUllmann <johannes-ullmann@web.de>
2024-07-07 19:02:01 +0200

    Skripte fertig kommentiert

--- VocabVR_VRRoom/Assets/BodyCollision.cs
+++ VocabVR_VRRoom/Assets/BodyCollision.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class BodyCollision : MonoBehaviour
 {
 
-    public Transform head;
-    public Transform feet;
-    // Start is called before the first frame update
+    public Transform head; //Referenz auf den Kopf des Charakters
+    public Transform feet; //Referenz auf die Füße des Charakters
+  
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    // Update wird jeden Frame ausgeführt
     void Update()
     {
-        gameObject.transform.position = new Vector3(head.position.x, feet.position.y, head.position.z);
+        gameObject.transform.position = new Vector3(head.position.x, feet.position.y, head.position.z); // Setzt die Position des GameObjects basierend auf den Positionen von Kopf und Füßen
 
     }
 }
--- VocabVR_VRRoom/Assets/Johannes_School_Asset/FacePlayerVR.cs
+++ VocabVR_VRRoom/Assets/Johannes_School_Asset/FacePlayerVR.cs
@@ -11,16 +11,16 @@ public class FacePlayerVR : MonoBehaviour
         // Überprüfen, ob die Spieler-Kamera gesetzt ist
         if (playerCamera != null)
         {
-            // Berechnen Sie die Richtung von diesem Objekt zur Spieler-Kamera
+            // Berechnen der Richtung von diesem Objekt zur Spieler-Kamera
             Vector3 direction = playerCamera.position - transform.position;
 
-            // Ignorieren Sie die Y-Achse, um ein Kippen zu verhindern
+            // Ignorieren der Y-Achse, um ein Kippen zu verhindern
             direction.y = 0;
 
-            // Berechnen Sie die Rotation, die dieses Objekt in Richtung der Kamera zeigt
+            // Berechnen der Rotation, die dieses Objekt in Richtung der Kamera zeigt
             Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
 
-            // Setzen Sie die Rotation dieses Objekts
+            // Setzen der Rotation dieses Objekts
             transform.rotation = rotation;
         }
     }
